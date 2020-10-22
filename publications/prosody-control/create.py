646c3bd2343157133412d7e6653ef3f8bc50333a
Nikos Ellinas <n.ellinas@samsung.com>
2020-10-22 17:51:46 +0300

    add grid
    Signed-off-by: Nikos Ellinas <n.ellinas@samsung.com>

--- publications/prosody-control/create.py
+++ publications/prosody-control/create.py
@@ -123,6 +123,60 @@ for fname in ['10-AWK', '10-CHS', '10-ECO']:
     out.write('</div>\n')
 out.write('</div>\n')
 
+# Joint model
+out.write('<h2 style="text-align: left;">Joint model modification based on offset from ground truth labels</h2>\n')
+mypath = f'audio/joint-offset-mod'
+files = sorted([f for f in listdir(mypath) if isfile(join(mypath, f))])
+out.write('<div class="sample-container">\n')
+for fname in ['10-AWK', '10-CHS', '10-ECO']:
+    out.write('<div class="sample">\n')
+    out.write('<div class="sample-title">\n')
+    out.write('<span class="quotation">&ldquo;</span>\n')
+    out.write('<div class="transcript">')
+    out.write(transcript[fname])
+    out.write('</div>\n')
+    out.write('</div>\n')
+    # GT
+    out.write('<div class="mod-container">\n')
+    out.write('<div class="sample-audio">\n')
+    out.write(f'<div class="gt" onclick="togglePlay(document.getElementById(\'joint_mod_{fname}_gt\'))">Ground Truth</div>\n')
+    out.write(f'<audio id="joint_mod_{fname}_gt" controls>\n')
+    out.write(f'<source src="{mypath}/{fname}.wav" type="audio/wav">\n')
+    out.write('Your browser does not support the audio element.\n')
+    out.write('</audio>\n')
+    out.write('</div>\n')
+    out.write('</div>\n')
+    # Minus
+    rang = 4
+    for moda1 in ['dum'] + [f'-{i}' for i in list(range(rang, 0, -1))] + ['+0'] + [f'+{i}' for i in list(range(1, rang + 1))]:
+        out.write('<div class="mod-container">\n')
+        out.write('<div class="sample-audio">\n')
+        if moda1 == '+0':
+            out.write(f'<div class="invisible axis">F0</div>\n')
+        else:
+            out.write(f'<div class="invisible"></div>\n')
+        out.write('</div>\n')
+        for moda2 in [f'-{i}' for i in list(range(rang, 0, -1))] + ['+0'] + [f'+{i}' for i in list(range(1, rang + 1))]:
+            if moda1 == 'dum':
+                out.write('<div class="sample-audio">\n')
+                if moda2 == '+0':
+                    out.write(f'<div class="invisible axis">Dur</div>\n')
+                else:
+                    out.write(f'<div class="invisible"></div>\n')
+                out.write('</div>\n')
+            else:
+                out.write('<div class="sample-audio">\n')
+                out.write(f'<div class="r-number-aug" onclick="togglePlay(document.getElementById(\'joint_mod_{fname}_{moda1}_{moda2}\'))">{moda1}_{moda2}</div>\n')
+                out.write(f'<audio id="joint_mod_{fname}_{moda1}_{moda2}" controls>\n')
+                out.write(f'<source src="{mypath}/{fname}_{moda1}_{moda2}.wav" type="audio/wav">\n')
+                out.write('Your browser does not support the audio element.\n')
+                out.write('</audio>\n')
+                out.write('</div>\n')
+        out.write('</div>\n')
+
+    out.write('</div>\n')
+out.write('</div>\n')
+
 out.write('</body>\n')
 out.write('</html>\n')
 out.close()
