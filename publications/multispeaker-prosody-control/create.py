9d872569713a52c4640761862376290a80c6d233
Nikos Ellinas <n.ellinas@samsung.com>
2021-04-16 12:26:19 +0300

    word augm
    Signed-off-by: Nikos Ellinas <n.ellinas@samsung.com>

--- publications/multispeaker-prosody-control/create.py
+++ publications/multispeaker-prosody-control/create.py
@@ -397,8 +397,155 @@ for fname in ['567-RWV', '373-SUM']:
 
 out.write('</div>\n')
 
+# Word augmentation
+out.write('<a href="#title5" class="custom-a-href"><h2 style="text-align: left;" onclick="showSample(event)" id="title1">5) Single word augmentation</h2></a>\n')
+mypath = f'audio/word'
+out.write('<div class="sample-container" style="display:none">\n')
+
+out.write('<h3>Multispeaker/speaker adaptation same voice comparison</h3>')
+ph_dict = {
+    '3196-PER': '01',
+}
+for fname in ['3196-PER', '3196-PER2']:
+    if fname == '3196-PER2':
+        fname = '3196-PER'
+        ph_dict[fname] = '03'
+        trans = transcript[fname].split()
+        trans[3] = '<span style="color:red">acknowledged</span>'
+    else:
+        trans = transcript[fname].split()
+        trans[1] = '<span style="color:red">Wentworth</span>'
+    out.write('<div class="sample">\n')
+    out.write('<div class="sample-title">\n')
+    out.write('<span class="quotation">&ldquo;</span>\n')
+    out.write('<div class="transcript">')
+    out.write(' '.join(trans))
+    out.write('</div>\n')
+    out.write('</div>\n')
+    # GT
+    spk_name = 'cathy-multi'
+    orig_speaker = spk_name.split('-')[0]
+    out.write('<div class="mod-container">\n')
+    out.write('<div class="sample-audio">\n')
+    out.write(f'<div class="gt" onclick="togglePlay(document.getElementById(\'word_{fname}_gt_{spk_name}\'))">Ground Truth</div>\n')
+    out.write(f'<audio id="word_{fname}_gt_{spk_name}" controls preload="none">\n')
+    out.write(f'<source src="audio/gt/{orig_speaker}_{fname}_gt_gt.wav" type="audio/wav">\n')
+    out.write('Your browser does not support the audio element.\n')
+    out.write('</audio>\n')
+    out.write('</div>\n')
+    out.write('</div>\n')
+    for spk_name in ['cathy-multi', 'cathy-adapt']:
+        orig_speaker = spk_name.split('-')[0]
+        # Mods per speaker
+        out.write('<div class="mod-container">\n')
+        for moda in [spk_name] + ['F0'] + [f'{i}' for i in range(0, 15)]:
+            out.write('<div class="sample-audio">\n')
+            if moda == spk_name:
+                out.write(f'<div class="invisible axis">{spk_name}</div>\n')
+            elif moda == 'F0':
+                out.write(f'<div class="invisible axis" style="width:48px;background-color:rgb(160,160,253)">F0</div>\n')
+            else:
+                out.write(f'<div class="r-number" onclick="togglePlay(document.getElementById(\'word_f0_{fname}_{ph_dict[fname]}_{moda}_{spk_name}\'))">{moda}</div>\n')
+                out.write(f'<audio id="word_f0_{fname}_{ph_dict[fname]}_{moda}_{spk_name}" controls preload="none">\n')
+                out.write(f'<source src="{mypath}/{spk_name}_{fname}_gt_f0_w_{ph_dict[fname]}_t_{int(moda):02d}.wav" type="audio/wav">\n')
+                out.write('Your browser does not support the audio element.\n')
+                out.write('</audio>\n')
+            out.write('</div>\n')
+        out.write('</div>\n')
+        out.write('<div class="mod-container">\n')
+        for moda in [spk_name] + ['Dur'] + [f'{i}' for i in range(0, 15)]:
+            out.write('<div class="sample-audio">\n')
+            if moda == spk_name:
+                out.write(f'<div class="invisible"></div>\n')
+            elif moda == 'Dur':
+                out.write(f'<div class="invisible axis" style="width:48px;background-color:rgb(160,160,253)">Dur</div>\n')
+            else:
+                out.write(f'<div class="r-number" onclick="togglePlay(document.getElementById(\'word_dur_{fname}_{ph_dict[fname]}_{moda}_{spk_name}\'))">{moda}</div>\n')
+                out.write(f'<audio id="word_dur_{fname}_{ph_dict[fname]}_{moda}_{spk_name}" controls preload="none">\n')
+                out.write(f'<source src="{mypath}/{spk_name}_{fname}_gt_duration_w_{ph_dict[fname]}_t_{int(moda):02d}.wav" type="audio/wav">\n')
+                out.write('Your browser does not support the audio element.\n')
+                out.write('</audio>\n')
+            out.write('</div>\n')
+        out.write('</div>\n')
+    out.write('</div>\n')
+
+out.write('<h3>Speaker adaptation</h3>')
+gtshow = 1
+ph_dict = {
+    '567-RWV': '01',
+    '978-SUM': '03'
+}
+for fname in ['567-RWV', '978-SUM']:
+    out.write('<div class="sample">\n')
+    out.write('<div class="sample-title">\n')
+    out.write('<span class="quotation">&ldquo;</span>\n')
+    out.write('<div class="transcript">')
+    trans = transcript[fname].split()
+    if fname == '567-RWV':
+        trans[1] = '<span style="color:red">father</span>'
+    else:
+        trans[3] = '<span style="color:red">unaware</span>'
+    out.write(' '.join(trans))
+    out.write('</div>\n')
+    out.write('</div>\n')
+    # GT
+    if gtshow == 1:
+        out.write('<div class="mod-container">\n')
+        for spk_name in ['dum'] + ['gt'] + ['obama', 'jsj', 'lj', 'martha']:
+            out.write('<div class="sample-audio">\n')
+            if spk_name == 'dum':
+                out.write(f'<div class="invisible"></div>\n')
+            elif spk_name == 'gt':
+                out.write(f'<div class="invisible axis">Ground Truth</div>\n')
+            else:
+                orig_speaker = spk_name.split('-')[0]
+                out.write(f'<div class="gt speaker" onclick="togglePlay(document.getElementById(\'word_{fname}_gt_{spk_name}\'))">{spk_name}</div>\n')
+                out.write(f'<audio id="word_{fname}_gt_{spk_name}" controls preload="none">\n')
+                out.write(f'<source src="audio/gt/{gt[spk_name]}" type="audio/wav">\n')
+                out.write('Your browser does not support the audio element.\n')
+                out.write('</audio>\n')
+            out.write('</div>\n')
+        out.write('</div>\n')
+        gtshow = 0
+    for spk_name in ['obama-adapt', 'jsj-adapt', 'lj-adapt', 'martha-adapt']:
+        orig_speaker = spk_name.split('-')[0]
+        # Mods per speaker
+        out.write('<div class="mod-container">\n')
+        for moda in [spk_name] + ['F0'] + [f'{i}' for i in range(0, 15)]:
+            out.write('<div class="sample-audio">\n')
+            if moda == spk_name:
+                out.write(f'<div class="invisible axis">{spk_name}</div>\n')
+            elif moda == 'F0':
+                out.write(f'<div class="invisible axis" style="width:48px;background-color:rgb(160,160,253)">F0</div>\n')
+            else:
+                out.write(f'<div class="r-number" onclick="togglePlay(document.getElementById(\'word_f0_{fname}_{ph_dict[fname]}_{moda}_{spk_name}\'))">{moda}</div>\n')
+                out.write(f'<audio id="word_f0_{fname}_{ph_dict[fname]}_{moda}_{spk_name}" controls preload="none">\n')
+                out.write(f'<source src="{mypath}/{spk_name}_{fname}_gt_f0_w_{ph_dict[fname]}_t_{int(moda):02d}.wav" type="audio/wav">\n')
+                out.write('Your browser does not support the audio element.\n')
+                out.write('</audio>\n')
+            out.write('</div>\n')
+        out.write('</div>\n')
+        out.write('<div class="mod-container">\n')
+        for moda in [spk_name] + ['Dur'] + [f'{i}' for i in range(0, 15)]:
+            out.write('<div class="sample-audio">\n')
+            if moda == spk_name:
+                out.write(f'<div class="invisible"></div>\n')
+            elif moda == 'Dur':
+                out.write(f'<div class="invisible axis" style="width:48px;background-color:rgb(160,160,253)">Dur</div>\n')
+            else:
+                out.write(f'<div class="r-number" onclick="togglePlay(document.getElementById(\'word_dur_{fname}_{ph_dict[fname]}_{moda}_{spk_name}\'))">{moda}</div>\n')
+                out.write(f'<audio id="word_dur_{fname}_{ph_dict[fname]}_{moda}_{spk_name}" controls preload="none">\n')
+                out.write(f'<source src="{mypath}/{spk_name}_{fname}_gt_duration_w_{ph_dict[fname]}_t_{int(moda):02d}.wav" type="audio/wav">\n')
+                out.write('Your browser does not support the audio element.\n')
+                out.write('</audio>\n')
+            out.write('</div>\n')
+        out.write('</div>\n')
+    out.write('</div>\n')
+
+out.write('</div>\n')
+
 # Phoneme augmentation
-out.write('<a href="#title4" class="custom-a-href"><h2 style="text-align: left;" onclick="showSample(event)" id="title1">6) Single phoneme augmentation (SAMPA representation)</h2></a>\n')
+out.write('<a href="#title6" class="custom-a-href"><h2 style="text-align: left;" onclick="showSample(event)" id="title1">6) Single phoneme augmentation (SAMPA representation)</h2></a>\n')
 mypath = f'audio/phoneme'
 out.write('<div class="sample-container" style="display:none">\n')
 
