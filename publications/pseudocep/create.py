510ecdd8aa1659f5e7e4361dcbad972937a93311
Nikos Ellinas <n.ellinas@samsung.com>
2025-09-16 16:58:06 +0300

    add pitchmod paper
    Signed-off-by: Nikos Ellinas <n.ellinas@samsung.com>

---
+++ publications/pseudocep/create.py
@@ -0,0 +1,15 @@
+import os
+from os import listdir
+from os.path import isfile, join
+import re
+
+
+os.system('cat template.html > index.html')
+out = open('index.html', 'a')
+
+out.write('</div>\n')
+
+# End
+out.write('</body>\n')
+out.write('</html>\n')
+out.close()
